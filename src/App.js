48fa5a70a57cd484a44b6e474ee3b970557f57c4
חן פלג <chenpeleg@gmail.com>
2020-05-19 19:43:36 +0300

    new app with new create react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import bilgi from './bilgi.json';
+import info from './info.json';
 import Experience from "./Experience";
 import Interests from "./Interets";
 import Projects from "./Projects";
@@ -13,7 +13,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            bilgi: {
+            info: {
                 "Experiences": [
                     {
                         "baslik": "",
@@ -79,8 +79,8 @@ class App extends Component {
     }
     componentDidMount() {
 
-        this.setState({ bilgi: bilgi })
-        console.log(bilgi)
+        this.setState({ info: info })
+        console.log(info)
     }
 
 
@@ -89,37 +89,37 @@ class App extends Component {
             <div className="wrapper">
                 <div className="sidebar-wrapper">
                     <div className="profile-container">
-                        <img className="profile" src={this.state.bilgi.image} alt=""
+                        <img className="profile" src={this.state.info.image} alt=""
                             width="100px" height="100px" />
-                        <h1 className="name"> {this.state.bilgi.name} </h1>
+                        <h1 className="name"> {this.state.info.name} </h1>
                         <h3 className="tagline">Developer</h3>
                     </div>
 
                     <div className="contact-container container-block">
                         <ul className="list-unstyled contact-list">
-                            <li className="email"><i className="fa fa-envelope"></i><a href={"mailto:" + this.state.bilgi.email}>{this.state.bilgi.email}</a></li>
-                            <li className="phone"><i className="fa fa-phone"></i><a href={this.state.bilgi.phone}>{this.state.bilgi.phone}</a></li>
-                            <li className="website"><i className="fa fa-globe"></i><a href={this.state.bilgi.website} target="_blank">{this.state.bilgi.website}</a></li>
-                            <li className="twitter"><i className="fa fa-twitter"></i><a href={this.state.bilgi.twitterlink} target="_blank">{this.state.bilgi.twitter}</a></li>
+                            <li className="email"><i className="fa fa-envelope"></i><a href={"mailto:" + this.state.info.email}>{this.state.info.email}</a></li>
+                            <li className="phone"><i className="fa fa-phone"></i><a href={this.state.info.phone}>{this.state.info.phone}</a></li>
+                            <li className="website"><i className="fa fa-globe"></i><a href={this.state.info.website} target="_blank">{this.state.info.website}</a></li>
+                            <li className="twitter"><i className="fa fa-twitter"></i><a href={this.state.info.twitterlink} target="_blank">{this.state.info.twitter}</a></li>
                         </ul>
                     </div>
                     <div className="education-container container-block">
                         <h2 className="container-block-title">Education</h2>
-                        {this.state.bilgi.Education.map((item, key) => <Education item={item} key={key} />)}
+                        {this.state.info.Education.map((item, key) => <Education item={item} key={key} />)}
                     </div>
 
 
                     <div className="languages-container container-block">
                         <h2 className="container-block-title">Languages</h2>
                         <ul className="list-unstyled interests-list">
-                            {this.state.bilgi.Languages.map((item, key) => <Language item={item} key={key} />)}
+                            {this.state.info.Languages.map((item, key) => <Language item={item} key={key} />)}
                         </ul>
                     </div>
 
                     <div className="interests-container container-block">
                         <h2 className="container-block-title">Interests</h2>
                         <ul className="list-unstyled interests-list">
-                            {this.state.bilgi.Interests.map((item, key) => <Interests item={item} key={key} />)}
+                            {this.state.info.Interests.map((item, key) => <Interests item={item} key={key} />)}
                         </ul>
                     </div>
 
@@ -131,14 +131,14 @@ class App extends Component {
                     <section className="section summary-section">
                         <h2 className="section-title"><i className="fa fa-user"></i>Career Profile</h2>
                         <div className="summary">
-                            <p>{this.state.bilgi.Career}</p>
+                            <p>{this.state.info.Career}</p>
                         </div>
                     </section>
 
                     <section className="section experiences-section">
                         <h2 className="section-title"><i className="fa fa-briefcase"></i>Experiences</h2>
 
-                        {this.state.bilgi.Experiences.map((item, key) => <Experience item={item} key={key} />)}
+                        {this.state.info.Experiences.map((item, key) => <Experience item={item} key={key} />)}
 
                     </section>
 
@@ -146,15 +146,15 @@ class App extends Component {
                     <section className="section projects-section">
                         <h2 className="section-title"><i className="fa fa-archive"></i>Projects</h2>
                         <div className="intro">
-                            <p>{this.state.bilgi.ProjectDescription}</p>
+                            <p>{this.state.info.ProjectDescription}</p>
                         </div>
-                        {this.state.bilgi.Projects.map((item, key) => <Projects item={item} key={key} />)}
+                        {this.state.info.Projects.map((item, key) => <Projects item={item} key={key} />)}
                     </section>
 
                     <section className="skills-section section">
                         <h2 className="section-title"><i className="fa fa-rocket"></i>Skills &amp; Proficiency</h2>
                         <div className="skillset">
-                            {this.state.bilgi.Skills.map((item, key) => <Skill item={item} key={key} />)}
+                            {this.state.info.Skills.map((item, key) => <Skill item={item} key={key} />)}
                         </div>
                     </section>
 

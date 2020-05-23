2c680cf503ad0673bbed2bb921240d149ebf0555
חן פלג <chenpeleg@gmail.com>
2020-05-23 13:16:22 +0300

    Updates

---
+++ precache-manifest.b1ea845e4cde8d0b241dbf11277e260d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691e35dc916736965e07a9a02e7ce5f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf2e5a4e4e5c1da0fd3",
+    "url": "/static/css/main.55801b03.chunk.css"
+  },
+  {
+    "revision": "8d081ec2cc197c0e9c7c",
+    "url": "/static/js/2.87cd5c6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87cd5c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf2e5a4e4e5c1da0fd3",
+    "url": "/static/js/main.82ed973d.chunk.js"
+  },
+  {
+    "revision": "b38f709691f005f4c585",
+    "url": "/static/js/runtime-main.e2af1984.js"
+  }
+]);

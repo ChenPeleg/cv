8acd631a114d13a4b028c59a09b37d60cb87c432
Chen Peleg <chenpeleg@gmail.com>
2025-11-26 17:01:48 +0200

    feat: enhance AppLayout with flexbox layout and add shadow effect

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -5,7 +5,8 @@ import {AppSidebar} from './AppSidebar.tsx';
 
 export const AppLayout = ({info}: { info: Information }) => {
 
-    return <div className="wrapper bg-light max-w-[960px] my-0 mx-auto relative"  >
+    return <div className={`wrapper bg-light max-w-[960px] my-0 mx-auto relative flex lg:flex-row flex-col`}
+                style={{boxShadow: "0px 2px 4px 2px rgba(0, 0, 0, 0.4)"}}>
         <ExportButtons></ExportButtons>
         <AppSidebar info={info}/>
         <AppMainContent info={info}/>

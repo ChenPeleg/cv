afd8716a4264f55f31da3de662f63190ff703c43
Chen Peleg <chenpeleg@gmail.com>
2025-11-27 17:11:49 +0200

    feat: add vitest testing framework and configure initial tests

---
+++ src/pdf/useDownloadAsPdf.tsx
@@ -0,0 +1,7 @@
+export const useDownloadAsPdf = () => {
+    const downloadAsPdf = () => {
+        // Functionality to download content as PDF would be implemented here
+        console.log("Download as PDF functionality is not yet implemented.");
+    }
+    return  {downloadAsPdf}
+}

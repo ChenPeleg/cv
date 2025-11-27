2194da9f6286002e74e1f7fbb1f2c67c7bacbecd
Chen Peleg <chenpeleg@gmail.com>
2025-11-27 17:18:27 +0200

    feat: update ExportButtons to conditionally render PDF download button for localhost

--- src/components/Bottons/ExportButtons.tsx
+++ src/components/Bottons/ExportButtons.tsx
@@ -5,7 +5,9 @@ import {useDownloadAsPdf} from '../../pdf/useDownloadAsPdf.tsx';
 
 const ExportButtons: React.FC<any> = () => {
     const {downloadAsPdf} = useDownloadAsPdf()
+    const newPDfDownload = location.href.includes('localhost')
     const print = () => {
+        console.log(location.href );
         if (location.href.includes('localhost')) {
             return downloadAsPdf()
         }
@@ -14,8 +16,10 @@ const ExportButtons: React.FC<any> = () => {
 
     return (
         <div className="buttons-wrapper">
-            <a className="button" href={pdfCv} target="_blank" download title="Download CV" rel="noopener noreferrer"> <i
-                className="fa fa-download"></i> </a>
+            {!newPDfDownload  ? (    <a className="button" href={pdfCv} target="_blank" download title="Download CV" rel="noopener noreferrer"> <i
+                className="fa fa-download"></i> </a>) : (   <div className="button" onClick={downloadAsPdf} title="Download CV as PDF">
+                <i className="fa fa-download"></i></div>)}
+
             <div className="button" onClick={print} title="Print CV">
                 <i className="fa fa-print"></i></div>
         </div>);

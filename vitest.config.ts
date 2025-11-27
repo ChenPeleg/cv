535aa6a121918dc77b8a078a0cfdd44f2df3d6f0
Chen Peleg <chenpeleg@gmail.com>
2025-11-27 17:03:15 +0200

    feat: add vitest testing framework and configure initial tests

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+// @ts-ignore
+import react from '@vitejs/plugin-react';
+
+
+export default defineConfig({
+    plugins: [ react()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        includeSource: ["src/**/*.{js,jsx,ts,tsx}"],
+        exclude: ["e2e", "node_modules", "dist", ".idea", ".git", ".cache"],
+    },
+});

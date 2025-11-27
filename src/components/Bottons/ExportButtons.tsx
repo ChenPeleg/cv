2625aecce7fc57be82ff77e8e1dc42314e8b9586
Chen Peleg <chenpeleg@gmail.com>
2025-11-27 17:14:38 +0200

    feat: refactor ExportButtons component to functional style and streamline print functionality

--- src/components/Bottons/ExportButtons.tsx
+++ src/components/Bottons/ExportButtons.tsx
@@ -1,27 +1,21 @@
-import { Component } from 'react';
+import React from "react";
 import pdfCv from "../../assets/ChenPelegCV.pdf"
 
 
-class ExportButtons extends Component<any, any> {
-
-  download() {
-    alert("down")
-  }
-  print = () => {
+const ExportButtons: React.FC<any> = () => {
+  const print = () => {
    if (location.href.includes("localhost")) {
 
    }
-    window.print()
-  }
-  render() {
-    return (
-      <div className="buttons-wrapper">
-        <a className="button" href={pdfCv} target="_blank" download title="Download CV" rel="noopener noreferrer"> <i className="fa fa-download"></i> </a>
-        <div className="button" onClick={this.print} title="Print CV">
-          <i className="fa fa-print"></i></div >
-      </div >);
-  }
-}
+    window.print();
+  };
+
+  return (
+    <div className="buttons-wrapper">
+      <a className="button" href={pdfCv} target="_blank" download title="Download CV" rel="noopener noreferrer"> <i className="fa fa-download"></i> </a>
+      <div className="button" onClick={print} title="Print CV">
+        <i className="fa fa-print"></i></div >
+    </div >);
+};
 
 export default ExportButtons;
-
